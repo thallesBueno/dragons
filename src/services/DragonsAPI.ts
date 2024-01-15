ef9389d62de7c865d3f6caab43f3d05efa8d0055
Thalles Bueno <thalles.bueno@topaziopartner.com.br>
2024-01-15 13:34:38 -0300

    change API URL to env

--- src/services/DragonsAPI.ts
+++ src/services/DragonsAPI.ts
@@ -1,12 +1,16 @@
+'use client'
+
 import axios from "axios"
 import { Dragon } from "@/entities";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon'
+  baseURL: process.env.NEXT_PUBLIC_DRAGONS_API_BASE_URL
 });
 
 const getDragons = async () => {
   const response = await axiosInstance.get<Dragon[]>('/');
+
+  console.log(response)
 
   return response.data;
 }
